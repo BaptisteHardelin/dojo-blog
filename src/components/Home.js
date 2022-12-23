526556a2547ca7c4e9abca8a42bc75a36ce8a7bb
BHardelin <baptiste.hardelin@adeo.com>
2022-12-23 23:41:31 +0100

    Add clicks events

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,18 @@
 const Home = () => {
+  const handleClick = () => {
+    console.log("Hello ninjas");
+  };
+
+  const handleClickAgain = (name) => {
+    console.log("Hello " + name);
+  };
+
   return (
     <div className="home">
       <h2>HomePage</h2>
+
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain("mario")}>Click me again</button>
     </div>
   );
 };
