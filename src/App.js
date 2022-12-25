a24ad475f055db4eb130cd230ecd76e258e73bc5
BHardelin <baptiste.hardelin@adeo.com>
2022-12-25 15:05:00 +0100

    Can go home by clicking on the leftest link of the nav and add a way to acessing to the detail of a blog

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import NavBar from "./view/Navbar";
 import Home from "./view/Home";
 import Create from "./view/Create";
+import BlogDetails from "./view/BlogDetails";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -12,6 +13,7 @@ const App = () => {
           <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/create" element={<Create />} />
+            <Route exact path="/blogs/:id" element={<BlogDetails />} />
           </Routes>
         </div>
       </div>
