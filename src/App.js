980560e5a4d4a3e90ec5917fd20ba90ecaa2832b
BHardelin <baptiste.hardelin@adeo.com>
2022-12-25 14:02:21 +0100

    Add react router dom and some routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import NavBar from "./view/Navbar";
 import Home from "./view/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div className="App">
-      <NavBar />
-      <div className="content">
-        <Home />
+    <Router>
+      <div className="App">
+        <NavBar />
+        <div className="content">
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/create" />
+          </Routes>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 };
 
