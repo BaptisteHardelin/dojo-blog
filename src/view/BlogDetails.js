110bcb7c5ac1dc1400c84a71a6479a5d87bc857c
BHardelin <baptiste.hardelin@adeo.com>
2022-12-25 16:19:13 +0100

    Add deleted blog method

--- src/view/BlogDetails.js
+++ src/view/BlogDetails.js
@@ -1,13 +1,23 @@
 import { useParams } from "react-router";
+import { useNavigate } from "react-router-dom";
 import useFetch from "./UseFetch";
 
 const BlogDetails = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   const {
     data: blog,
     error,
     isPending,
   } = useFetch(`http://localhost:8000/blogs/${id}`);
+
+  const handleClick = () => {
+    fetch(`http://localhost:8000/blogs/${blog.id}`, {
+      method: "DELETE",
+    }).then(() => {
+      navigate("/");
+    });
+  };
 
   return (
     <div className="blog-details">
@@ -18,6 +28,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleClick}>Delete</button>
         </article>
       )}
     </div>
