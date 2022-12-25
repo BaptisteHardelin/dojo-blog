53d49ba6f5318353cd3418c3d4a38b35fb658f00
BHardelin <baptiste.hardelin@adeo.com>
2022-12-25 16:29:03 +0100

    Add a 404 page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import NavBar from "./view/Navbar";
 import Home from "./view/Home";
 import Create from "./view/Create";
 import BlogDetails from "./view/BlogDetails";
+import NotFound from "./view/NotFound";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 const App = () => {
@@ -14,6 +15,9 @@ const App = () => {
             <Route exact path="/" element={<Home />} />
             <Route exact path="/create" element={<Create />} />
             <Route exact path="/blogs/:id" element={<BlogDetails />} />
+
+            {/* {Always in the bottom !!! for the not found page (404)} */}
+            <Route exact path="*" element={<NotFound />} />
           </Routes>
         </div>
       </div>
